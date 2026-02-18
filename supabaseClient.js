10b730c126fcaf82f6e1e82657c0d5ee25ce8f58
Tyon <82318775+MAGUSTYON@users.noreply.github.com>
2026-02-18 23:33:39 +0800

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const SUPABASE_URL = "https://pxjnrkkkaznebjtxmuqp.supabase.co";
-export const SUPABASE_ANON_KEY = "ISI_PUBLISHABLE_KEY_DI_SINI"; // harus sb_publishable_...
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4am5ya2trYXpuZWJqdHhtdXFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1MjIyNDcsImV4cCI6MjA4NjA5ODI0N30.w88N_rvPaLT7Eii0JC8AvgOlDNt-X8HjqCP6bq4w44o"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
@@ -11,3 +11,4 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     detectSessionInUrl: true,
   },
 });
+
